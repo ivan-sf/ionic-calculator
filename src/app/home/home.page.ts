941ece03be825c3ae19dfd5c590599bd47879e5c
MajorTom <ivansantander2020@gmail.com>
2022-03-06 12:47:55 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // calc:any = document.querySelector('input[name="calc"]');
+  calculate:any = "";
+  calculateAux:any = "";
+  n2:any = "";
+  n3:any = "";
+  result:any = "";
 
   constructor() {}
+  
+  calculator(val){
+    this.operation(val)
+    
+    setTimeout(() => {
+      if(val != '='){
+        this.n3 = this.calculate;
+        console.log("this.calculate1",this.calculate);
+      }else{
+        this.n2 = this.n3;
+        this.n3 = this.result;
+        this.calculate = this.result;
+        console.log("this.calculate2",this.calculate);
+      }
+    }, 1000);
+  }
+
+  operation(val){
+    let value = "";
+    (val == 'x')? value = '*' : value = val;
+    if(value == 'reset'){
+      this.calculate = '';
+    }
+    else if(value == '='){
+      this.result = eval(this.calculate);
+    }else{
+      this.calculate += value;
+    };
+  }
 
 }
